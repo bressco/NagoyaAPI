1372920d66357bf6a1c5e157a95e3a850a9ae190
bressco <constantin.bress@partner.kit.edu>
2025-10-02 10:23:23 +0200

    feat: add check whether probe is from nagoya implementing country

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,33 @@
-fn main() {}
+use serde::Deserialize;
+use std::collections::HashSet;
+use std::error::Error;
+
+// get countries
+#[derive(Deserialize)]
+struct ImplementingCountries {
+    countries: HashSet<String>,
+}
+
+/// Checks whether the probe is from a country implementing Nagoya Measures. If so, the Result
+/// contains true.
+///
+/// # Arguments
+///
+/// * `implementing_countries`: Countries implementing Nagoya measures.
+/// * `probe_country`: Country from where the probe was or is to be extracted
+///
+/// returns: Result<bool, Box<dyn Error, Global>>
+///
+/// # Examples
+///
+/// ```
+///
+/// ```
+async fn is_probe_in_implementing_country(
+    implementing_countries: &ImplementingCountries,
+    probe_country: &str,
+) -> Result<bool, Box<dyn Error>> {
+    // Check whether probe country is in list of implementing countries
+    Ok(implementing_countries.countries.contains(probe_country))
+}
+
