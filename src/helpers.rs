7a319695da025e31dddeed5f39859a6442a39d01
bressco <constantin.bress@partner.kit.edu>
2025-10-13 22:18:49 +0200

    feat: add fetching and parsing the treaty json from external source

--- src/helpers.rs
+++ src/helpers.rs
@@ -1,8 +1,102 @@
-use crate::models::ImplementingCountries;
+use crate::models::{ImplementingCountries, NagoyaCountryInfo};
+use std::collections::HashSet;
 use std::error::Error;
 
-pub fn get_implementing_countries() -> Result<ImplementingCountries, Box<dyn Error>> {
-    let v: ImplementingCountries =
-        serde_json::from_str(include_str!("../assets/nagoya_countries.json"))?;
+fn fetch_absch_treaty_info() -> String {
+    // Creates a new client on each call. As this call should happen rarely due to caching, this
+    // should be ok
+    // Just gets the json
+    let absch_json = reqwest::blocking::get("https://api.cbd.int/api/v2013/countries/");
+
+    // Error handling here instead of passing
+
+    match absch_json {
+        Ok(x) => {
+            match x.error_for_status() {
+                Ok(x) => {
+                    x.text().unwrap();
+                }
+                // Error on server side
+                Err(e) => {
+                    // TODO: Handle this cleaner
+                    eprintln!("Server returned an error: {}", e.status().unwrap())
+                }
+            }
+        }
+        // Error if for different reasons a connection cannot be established
+        // TODO: Act differently based on server error, e.g. try again with temp errors
+        Err(e) => {
+            eprintln!("Could not fetch data from ABSCH: {}", e)
+        }
+    }
+
+    // TODO: Make several attempts instead of just once and then giving up
+    // If a cache is used, that could be used instead, also to steer the retry time by resetting
+    // the stale "timer". But for now this should be enough
+    // At least, if the fetching fails repeatedly, maybe configurable in .env
+    panic!("Could not fetch data from ABSCH");
+}
+
+fn get_nagoya_treaty_info(absch_json: &str) -> Result<HashSet<NagoyaCountryInfo>, Box<dyn Error>> {
+    let v: HashSet<NagoyaCountryInfo> = serde_json::from_str(&absch_json)?;
     Ok(v)
+}
+
+pub fn get_implementing_countries() -> Result<ImplementingCountries, Box<dyn Error>> {
+    // TODO: Use Caching
+    // TODO: Instead of strings, use the data model provided by the iso3166 crate (or return a fitting error)
+
+    // Get JSON from ABSCH (if not in cache; cache duration in config)
+    let nagoya_country_info = get_nagoya_treaty_info(&fetch_absch_treaty_info())?;
+
+    // Get List of implementing countries from the struct. Assumed that those are the countries which
+    // are party to the contract
+    let code3_entries: Vec<String> = nagoya_country_info
+        .iter()
+        .filter(|country| country.treaties.nagoya.party_date.is_some())
+        .map(|country| country.code3.clone())
+        .collect();
+
+    let countries = ImplementingCountries {
+        countries: HashSet::from_iter(code3_entries),
+    };
+
+    Ok(countries)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::models::{Treaties, Treaty};
+
+    #[test]
+    fn test_get_nagoya_treaty_info() {
+        let testdata_json = r#"
+        [
+            {
+                "code2": "AD",
+                "code3": "AND",
+                "treaties": {
+                    "XXVII8":   { "party": "2015-05-05" },
+                    "XXVII8a":  { "party": null },
+                    "XXVII8b":  { "party": "2025-10-12" },
+                    "XXVII8c":  { "party": null }
+                }
+            }
+        ]
+        "#;
+        let country_data = NagoyaCountryInfo {
+            code3: String::from("AND"),
+            code2: String::from("AD"),
+            treaties: Treaties {
+                nagoya: Treaty {
+                    party_date: Some(String::from("2025-10-12")),
+                },
+            },
+        };
+        assert_eq!(
+            get_nagoya_treaty_info(&testdata_json).unwrap(),
+            HashSet::from_iter(vec!(country_data))
+        );
+    }
 }
--- src/models.rs
+++ src/models.rs
@@ -2,7 +2,28 @@ use serde::{Deserialize, Serialize};
 use std::collections::HashSet;
 use utoipa::{IntoParams, IntoResponses, ToSchema};
 
-// get countries
+#[derive(Deserialize, PartialEq, Hash, Eq, Debug)]
+pub struct Treaty {
+    #[serde(rename = "party")]
+    pub(crate) party_date: Option<String>,
+}
+
+#[derive(Deserialize, Hash, PartialEq, Eq, Debug)]
+pub struct Treaties {
+    //#[serde(rename = "XXVII8b", flatten)]
+    #[serde(rename = "XXVII8b")]
+    pub(crate) nagoya: Treaty,
+}
+
+#[derive(Deserialize, PartialEq, Hash, Eq, Debug)]
+pub struct NagoyaCountryInfo {
+    pub(crate) code2: String,
+    pub(crate) code3: String,
+    //pub(crate) nagoya_info: NagoyaTreatyInfo,
+    //#[serde(flatten)]
+    pub(crate) treaties: Treaties,
+}
+
 #[derive(Deserialize, Clone)]
 pub struct ImplementingCountries {
     pub(crate) countries: HashSet<String>,
