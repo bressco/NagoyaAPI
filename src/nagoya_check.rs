b49bf3532ec8fef872b59c91a602b436a60f366c
bressco <constantin.bress@partner.kit.edu>
2025-12-23 22:29:47 +0100

    chore: remove obsolete comments, tests

--- src/nagoya_check.rs
+++ src/nagoya_check.rs
@@ -2,21 +2,6 @@ use crate::models::{ImplementingCountries, NagoyaCheckData, NagoyaResponse};
 use axum::Json;
 use std::error::Error;
 
-/// Checks whether the probe is from a country implementing Nagoya Measures. If so, the Result
-/// contains true.
-///
-/// # Arguments
-///
-/// * `implementi pub(crate)ng_countries`: Countries implementing Nagoya measures.
-/// * `probe_country`: Country from where the probe was or is to be extracted
-///
-/// returns: Result<bool, Box<dyn Error, Global>>
-///
-/// # Examples
-///
-/// ```
-///
-/// ```
 pub async fn is_probe_in_implementing_country(
     implementing_countries: &ImplementingCountries,
     probe_country: &str,
@@ -37,30 +22,6 @@ pub async fn nagoya_check(
         .await
         .unwrap(),
     })
-}
-
-#[tokio::test]
-async fn test_are_affils_from_probe_country() {
-    //let data_included = HashSet::from_iter(vec!["AUS".to_string(), "DEU".to_string()]);
-    //let data_not_included = HashSet::from_iter(vec!["DEU".to_string()]);
-    //let data_empty = HashSet::from_iter(vec!["".to_string()]);
-    //let probe: &str = "AUS";
-
-    // assert!(
-    //     are_affils_from_probe_country(&data_included, &probe)
-    //         .await
-    //         .unwrap(),
-    // );
-    // assert!(
-    //     !are_affils_from_probe_country(&data_not_included, &probe)
-    //         .await
-    //         .unwrap(),
-    // );
-    // assert!(
-    //     !are_affils_from_probe_country(&data_empty, &probe)
-    //         .await
-    //         .unwrap()
-    // );
 }
 
 #[tokio::test]
