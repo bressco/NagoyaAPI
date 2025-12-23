66c71515ed7b4f0c34880600bf2f853f837ed996
bressco <constantin.bress@partner.kit.edu>
2025-12-23 22:23:36 +0100

    feat: add check based on geocoordinates, add swagger-ui

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,22 @@
+use crate::models::{AppState, Config, ImplementingCountries};
 use axum::Router;
 use axum::routing::{get, post};
 use dotenvy::dotenv;
+use std::collections::HashMap;
 use utoipa::OpenApi;
+use utoipa_swagger_ui::SwaggerUi;
 
-mod handlers;
-mod helpers;
+mod api;
+mod external_data;
 mod models;
 mod nagoya_check;
 
 #[derive(OpenApi)]
 #[openapi(paths(
-    handlers::openapi,
-    handlers::nagoya_check_wrapper,
-    handlers::health_check
+    api::openapi,
+    api::nagoya_check_country_code,
+    api::nagoya_check_geocoordinates,
+    api::health_check
 ))]
 pub struct ApiDoc;
 
@@ -20,26 +24,45 @@ pub struct ApiDoc;
 async fn main() {
     // Load env
     dotenv().ok();
+    let env_map: HashMap<String, String> = dotenvy::vars().collect();
 
     // Load List of Countries implementing measures according to the Nagoya Protocol
-    let implementing_countries = helpers::get_implementing_countries().await.unwrap();
+    // TODO: Use timestamped struct for caching
+    let implementing_countries: ImplementingCountries =
+        external_data::get_implementing_countries().await.unwrap();
 
-    let server_address = dotenvy::var("SERVER_HOST").unwrap_or_else(|_| "0.0.0.0".to_string());
-    let service_port = dotenvy::var("SERVER_PORT")
-        .unwrap_or_else(|_| "3000".to_string())
+    //let server_address = dotenvy::var("SERVER_HOST").unwrap_or_else(|_| "0.0.0.0".to_string());
+    let server_address = env_map.get("SERVER_HOST").unwrap();
+    let service_port = env_map
+        .get("SERVER_PORT") //dotenvy::var("SERVER_PORT")
+        //.unwrap_or_else(|_| "3000".to_string())
+        .unwrap()
         .parse::<u16>()
         .expect("Please select a valid port number of between 0 and 65535");
 
-    let listener = tokio::net::TcpListener::bind(format!("{}:{}", server_address, service_port))
-        .await
-        .unwrap();
+    let config = Config {
+        nominatim_host: env_map.get("NOMINATIM_HOST").unwrap().to_string(),
+        server_host: server_address.to_string(),
+        server_port: service_port,
+    };
+
+    let state = AppState {
+        implementing_countries,
+        config: config.clone(),
+    };
+
+    let listener =
+        tokio::net::TcpListener::bind(format!("{}:{}", config.server_host, config.server_port))
+            .await
+            .unwrap();
 
     let app = Router::new()
-        .route("/nagoya_check", post(handlers::nagoya_check_wrapper))
-        .route("/openapi.json", get(handlers::openapi))
-        .route("/health", get(handlers::health_check))
-        //.merge(utoipa_swagger_ui::SwaggerUi::new("/swagger-ui").url("/openapi.json", ApiDoc::openapi()))
-        .with_state(implementing_countries);
+        .route("/nagoya_check_cc", post(api::nagoya_check_country_code))
+        .route("/nagoya_check_geo", post(api::nagoya_check_geocoordinates))
+        .route("/openapi.json", get(api::openapi))
+        .route("/health", get(api::health_check))
+        .merge(SwaggerUi::new("/swagger-ui").url("/docs", ApiDoc::openapi()))
+        .with_state(state);
 
     axum::serve(listener, app).await.unwrap();
 }
